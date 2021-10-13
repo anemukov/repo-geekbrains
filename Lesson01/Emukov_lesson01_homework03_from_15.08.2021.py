d72458043ed6afdd0599839907f2337a5a5f0335
anemukov <88976541+anemukov@users.noreply.github.com>
2021-10-13 21:23:16 +0300

    Delete Lesson01 directory

--- Lesson01/Emukov_lesson01_homework03_from_15.08.2021.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Введите число "))
-summary = n + (n * 10 + n) + (n * 100 + n * 10 + n)
-print(summary)
--- Lesson01/Emukov_lesson01_homework04_from_15.08.2021.py
+++
@@ -1,7 +0,0 @@
-maxnumber = int(input("Введите целое число "))
-x = 0
-while maxnumber:
-    if maxnumber % 10 > x:
-        x = maxnumber % 10
-    maxnumber //= 10
-print(x)
